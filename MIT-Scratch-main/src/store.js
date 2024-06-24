f9ecb071f50e724ccd140144abff261ece85c969
srivardhan123 <srivardhanrao9009@gmail.com>
2024-06-24 22:36:40 +0530

    updated package versions

--- MIT-Scratch-main/src/store.js
+++ MIT-Scratch-main/src/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import { rootReducer } from "./redux/rootReducer";
-import thunk from "redux-thunk";
+import {thunk} from "redux-thunk";
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
